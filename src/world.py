ca82a8b734f27266c72b91b091f98c5b7d86642e
TitouanLeDilavrec <titouan.le.dilavrec@gmail.com>
2022-11-10 16:04:01 +0100

    modified:   src/world.py

--- src/world.py
+++ src/world.py
@@ -2,13 +2,29 @@ import pygame as pg
 from game_config import *
 from map import *
 
-class World: 
+class World:
+  """
+  World containing a map, props, mobs and players evolving in it,
+  and updating them.
+  """ 
   def __init__(self,game):
+    """
+    Spawns a Body.
+    
+    Input:
+        <none>
+    
+    Outputs:
+        World
+    """
     self.props = []
     self.mobs = []
     self.players = []
     self.map = Map(game)
 
   def update (self,game):
+    """
+    Makes the bodies evolve from 1 state to the next.
+    """
     pg.display.flip()
     game.clock.tick(GameConfig.FRAME_RATE)
