45d398359a8a37fa582fd9d945d4e56777af03fa
Le Dilavrec Titouan <titouan.ledilavrec@etudiant.univ-rennes1.fr>
2022-11-10 08:36:35 +0000

    Delete shell.nix

--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs;
-mkShell {
-  buildInputs = [
-    python310Packages.pygame # Nixpkgs fixes up some complexities
-    vulkan-loader # Griddly needs the Vulkan-SDK
-  ];
-  LD_LIBRARY_PATH="${vulkan-loader}/lib";
-}
