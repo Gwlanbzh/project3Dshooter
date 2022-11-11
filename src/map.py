9d0db4aad18eae57a49e74aa8a87aba90e97845e
Aodren Le Gloanec <aodren@vivo.LEGLO>
2022-11-11 21:39:38 +0100

    changed the map's structure and subsequently adapted the map class' methods

--- src/map.py
+++ src/map.py
@@ -9,19 +9,29 @@ from config import Config
 # 0 = False = vide
 # 1..n = Wall type
 # WAll type, will certainly be wall with different texture like reppresented on the top preview
+
+textures = {'d': "default"}
+
+# the map contains tuples (wall_type: int, texture: str), defined here.
+NO_W0 = (0, '')   # no wall
+W_SQ0 = (1, 'd')  # squared wall
+W_TR1 = (2, 'd')  # triangled wall nb 1
+W_TR2 = (3, 'd')  # triangled wall nb 2
+
+
 map = [
-    [2,1,2,1,1,3,3,3,1,1,1,1],
-    [1,0,0,0,0,0,0,0,0,0,0,1],
-    [3,0,0,1,0,0,0,1,1,1,0,1],
-    [1,3,1,1,0,0,0,1,0,0,0,1],
-    [1,0,0,0,0,2,0,0,0,0,0,1],
-    [1,0,2,2,2,0,2,2,0,0,1,1],
-    [1,0,0,0,0,0,0,0,0,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1]
+    [W_TR1, W_SQ0, W_TR1, W_SQ0, W_SQ0, W_TR2, W_TR2, W_TR2, W_SQ0, W_SQ0, W_SQ0, W_SQ0],
+    [W_SQ0, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, W_SQ0],
+    [W_TR2, NO_W0, NO_W0, W_SQ0, NO_W0, NO_W0, NO_W0, W_SQ0, W_SQ0, W_SQ0, NO_W0, W_SQ0],
+    [W_SQ0, W_TR2, W_SQ0, W_SQ0, NO_W0, NO_W0, NO_W0, W_SQ0, NO_W0, NO_W0, NO_W0, W_SQ0],
+    [W_SQ0, NO_W0, NO_W0, NO_W0, NO_W0, W_TR1, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, W_SQ0],
+    [W_SQ0, NO_W0, W_TR1, W_TR1, W_TR1, NO_W0, W_TR1, W_TR1, NO_W0, NO_W0, W_SQ0, W_SQ0],
+    [W_SQ0, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, NO_W0, W_SQ0, W_SQ0, W_SQ0],
+    [W_SQ0, W_SQ0, W_SQ0, W_SQ0, W_SQ0, W_SQ0, W_SQ0, W_SQ0, W_SQ0, W_SQ0, W_SQ0, W_SQ0]
 ]
 
 class Map:
-    def __init__(self,game):
+    def __init__(self, game):
         self.game = game
         self.map = map
         self.world = {}
@@ -35,24 +45,24 @@ class Map:
         '''
         for j,row in enumerate(self.map):
             for i, value in enumerate(row):
-                if value :
+                if value != NO_W0:
                     self.world[(i,j)] = value
   
-    def draw(self,game):
+    def draw(self, game):
         """
         temporary, waitting for true render false 3D
         pour rapple _RectValue c'est sous la forme : 
           Rect(left, top, width, height) -> Rect
         """
         for position in self.world:
-            if self.world[position] == 1:
+            if self.world[position][0] == 1: # index 0 extracts the type of wall.
                 pg.draw.rect(game.window,"black",
                               (position[0] * 100,position[1] * 100,100,100),2)
-            if self.world[position] == 2:
+            if self.world[position][0] == 2:
                 pg.draw.rect(game.window,"blue",
                               (position[0] * 100,position[1] * 100,100,100),2)
 
-            if self.world[position] == 3:
+            if self.world[position][0] == 3:
                 pg.draw.rect(game.window,"orange",
                               (position[0] * 100,position[1] * 100,100,100),2)
   
