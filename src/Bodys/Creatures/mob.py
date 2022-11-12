f34d07ebb82cd703a616de8bd574cbe442683b20
TitouanLeDilavrec <titouan.le.dilavrec@gmail.com>
2022-11-12 12:57:10 +0100

    truly basic movement for mob

--- src/Bodys/Creatures/mob.py
+++ src/Bodys/Creatures/mob.py
@@ -1,4 +1,5 @@
 from ..creature import Creature
+import math
 class Mob(Creature):
     def __init__(self,game,r):
         """
@@ -12,13 +13,34 @@ class Mob(Creature):
         """
         super().__init__(game,r)
         self.color = 'red' 
+        self.speed = 1
 
     def update(self):
         self.ia_command()
         pass
+
+    def movement(self):
+
+        x,y = self.r
+        target_pos = self.game.world.players[0].r
+        angle = math.atan2(target_pos[1] - y, target_pos[0] - x)
+        dx = math.cos(angle) * self.speed
+        dy = math.sin(angle) * self.speed
+
+        self.orientation = angle
+
+        x_permission, y_permission = self.not_colliding(dx, dy)
+        if x_permission:
+            x += dx
+        if y_permission:
+            y += dy 
+        self.r = x, y
+
+
     
     def ia_command(self):
         """
         Returns a force vector based on the move the IA choses.
         """
+        self.movement()
         pass
