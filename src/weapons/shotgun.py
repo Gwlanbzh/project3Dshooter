a11d8ee3378298cf7c990aac4b1c84f741efa1c7
Nicolas Moreau de Lizoreux <nicolas.delizoreux@etudiant.univ-rennes1.fr>
2022-12-16 08:54:01 +0100

    forgot to commit shotgun

--- src/weapons/shotgun.py
+++ src/weapons/shotgun.py
@@ -1,6 +1,7 @@
 from weapons import Weapon
 from config import *
 from math import pi
+import pygame as pg
 
 class Shotgun(Weapon):
 
@@ -10,9 +11,26 @@ class Shotgun(Weapon):
         self.range = WALL_WIDTH * 5
         self.dmg = 10
 
-    def hit_scan(self, pos, orientation, mob_list):
-        super().hit_scan(pos, orientation, mob_list)
-        super().hit_scan(pos, orientation - pi/4, mob_list)
-        super().hit_scan(pos, orientation + pi/4, mob_list)
+    def shoot(self, entity):
+        t = pg.time.get_ticks()
+        if t - self.last_shot_time > self.delay: # 100 ms between shots 
+            if entity.ammo > 0:
+                self.last_shot_time = t
 
-    
+                entity.ammo = max(0, entity.ammo - 3)
+
+                mob_list = entity.game.world.mobs
+                orien = entity.orientation
+
+                self.hit_scan(entity.game.world.map.map, entity.r, orien, mob_list)
+                self.hit_scan(entity.game.world.map.map, entity.r, orien - pi/6, mob_list)
+                self.hit_scan(entity.game.world.map.map, entity.r, orien + pi/6, mob_list)
+                self.play_sound()
+            
+            else:
+                self.play_sound(no_ammo=True)
+
+    #def hit_scan(self, pos, orientation, mob_list):
+    #    super().hit_scan(pos, orientation, mob_list)
+    #    super().hit_scan(pos, orientation - pi/4, mob_list)
+    #    super().hit_scan(pos, orientation + pi/4, mob_list)
