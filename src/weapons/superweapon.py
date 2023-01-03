17e36e64cb7e4238efc08e071b8568f0ed74317c
Nicosmonaute <nicoco1203moro@gmail.com>
2023-01-03 22:38:20 +0100

    started superweapon devlopment

--- src/weapons/superweapon.py
+++ src/weapons/superweapon.py
@@ -1,5 +1,24 @@
-from weapons.shotgun import Shotgun
+import pygame as pg
+from weapons.weapon import Weapon
+from config import *
+from render import load_superweapon
 
-class SuperWeapon(Shotgun):
+class SuperWeapon(Weapon):
     def __init__(self):
-        super().__init__()
+        super().__init__(self)
+
+        self.range = WALL_WIDTH * 15
+        self.dmg = 40
+        self.delay = 100 # ms
+        self.time_between_sprites = 50
+        self.image_index = 0
+
+        self.ammo_sound = [
+            pg.mixer.Sound(Config.SOUNDS_FOLDER + "weapons/fire_pistol.mp3"),
+        ]
+
+        self.no_ammo_sound = [ 
+            pg.mixer.Sound(Config.SOUNDS_FOLDER + "weapons/dryfire_rifle.mp3"),
+        ]
+
+        self.sprite = load_superweapon()
