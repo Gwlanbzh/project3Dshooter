fe99310af21a80fbd1dee14b7d994e9b0cb5e73f
Aodren Le Gloanec <loeiz.legloanec@etudiant.univ-rennes1.fr>
2023-01-04 14:49:23 +0100

    Improvement of game modes (end on death...), since the change seems to have been lost in a merge.

--- src/gamemodes.py
+++ src/gamemodes.py
@@ -3,12 +3,20 @@ from game import Game
 class Maze(Game):
     def is_game_over(self):
         curr_cell = (int(self.world.players[0].r.x)//100, int(self.world.players[0].r.y)//100)
-        return any([curr_cell == exit for exit in self.world.exits])
+        if any([curr_cell == exit for exit in self.world.exits]):
+            return "victory"
+        if self.world.players[0].health == 0:
+            return "defeat"
+        return ""
 
 class Boss_level(Game):
     def is_game_over(self):
-        return all([mob.health == 0 for mob in self.world.mobs])
+        if all([mob.health == 0 for mob in self.world.mobs]):
+            return "victory"
+        if self.world.players[0].health == 0:
+            return "defeat"
+        return ""
 
 if __name__ == "__main__":
-    game = Maze("assets/maps/map_dest.bin")
+    game = Maze("src/assets/maps/map_dest.bin", True)
     game.run()
