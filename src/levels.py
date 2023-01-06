19c40bd24c567bf42c38c2b3c10caaaa3fd193be
Aodren Le Gloanec <loeiz.legloanec@etudiant.univ-rennes1.fr>
2023-01-06 13:17:38 +0100

    Added placeholder for all maps and updated levels.py

--- src/levels.py
+++ src/levels.py
@@ -1,5 +1,8 @@
 from gamemodes import *
 
 levels = [
-    (Maze, "src/assets/maps/level_1.bin")
+    (Maze, "src/assets/maps/level_1.bin"),
+    (Maze, "src/assets/maps/level_2.bin"),
+    (Maze, "src/assets/maps/level_3.bin"),
+    (Boss_level, "src/assets/maps/boss.bin"),
 ]
