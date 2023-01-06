f2e4f51938cd9c886669a5a3b4443a183b0bb592
Aodren Le Gloanec <loeiz.legloanec@etudiant.univ-rennes1.fr>
2023-01-06 22:41:41 +0100

    sprites optimization

--- src/render/camera.py
+++ src/render/camera.py
@@ -138,12 +138,11 @@ class Camera():
                                  )
             
             for x in range(int(width)):
-                strip_index = int(x // px_per_stripe)
-                stripe = sprite[strip_index]
-                sprite_slice = pg.transform.scale(stripe, (1, height))
-                
                 i = int(draw_x + x)
                 if 0 <= i and i < len(z_buffer) and z_buffer[i] > distance:
+                    strip_index = int(x // px_per_stripe)
+                    stripe = sprite[strip_index]
+                    sprite_slice = pg.transform.scale(stripe, (1, height))
                     window.blit(sprite_slice, (i, draw_y))
     
     def draw_frame(self, window):
