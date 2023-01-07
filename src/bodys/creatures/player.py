3139780c610b43d5cdf04dce884df41518f170d1
nicolas.delizoreux@etudiant.univ-rennes1.fr <nicolas.delizoreux@etudiant.univ-rennes1.fr>
2023-01-07 23:57:01 +0100

    changed players.weapons list type to set type

--- src/bodys/creatures/player.py
+++ src/bodys/creatures/player.py
@@ -37,7 +37,7 @@ class Player(Creature):
 
         # weapons attributes
         self.current_weapon = Pistol()
-        self.weapons = [Punch, Pistol]
+        self.weapons = {Punch, Pistol}
         self.ammo = 20  # may change to dict ?
         
         self.max_ammo = 100
