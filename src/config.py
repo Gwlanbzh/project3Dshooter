b0e09c534286d6dbbaedf4b1ec4658bde1d21ad4
Aodren Le Gloanec <loeiz.legloanec@etudiant.univ-rennes1.fr>
2023-01-09 12:32:39 +0100

    Reverted FOV_X to 2pi/3. The previous was causing framereate drops issues.

--- src/config.py
+++ src/config.py
@@ -23,8 +23,8 @@ class Config():
     PLAYER_MOUSE_VERT_ROT_SPEED = 1
     
     WALL_HEIGHT = 200  # height of the wall that is above the player's point of view (e.g. for a height > VIEW_HEIGHT)
-    FOV_X = pi
-    FOV_Y = FOV_X * RES_Y / RES_X
+    FOV_X = 2*pi/3
+    FOV_Y = FOV_X
     
     VIEW_HEIGHT = 75
 
