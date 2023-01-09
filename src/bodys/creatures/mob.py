b21e6db269626d43839dafb4767727ffb5fe6008
Aodren Le Gloanec <loeiz.legloanec@etudiant.univ-rennes1.fr>
2023-01-09 20:46:55 +0100

    Balance changes

--- src/bodys/creatures/mob.py
+++ src/bodys/creatures/mob.py
@@ -188,7 +188,7 @@ class Grunt(Mob):
         super().__init__(game,r)
         
         self.size = 27
-        self.health = 100
+        self.health = 75
         self.current_weapon = Pistol()        # TODO add pistol
         # TODO implement dynamic sprites
         self.range = self.current_weapon.range
@@ -214,7 +214,7 @@ class Boss(Mob):
         super().__init__(game,r)
         
         self.size = 35
-        self.health = 2000
+        self.health = 3000
         self.current_weapon = SuperWeapon()        # TODO add pistol
         # TODO implement dynamic sprites
         self.range = self.current_weapon.range
